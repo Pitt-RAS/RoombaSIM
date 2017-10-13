ad183e22365dce92975fba02ee972d5885d5d641
Matt Bilker <me@mbilker.us>
2017-10-13 02:17:47 -0400

    Add HoldPositionTask to tasks dictionary

--- roombasim/pittras/config.py
+++ roombasim/pittras/config.py
@@ -14,6 +14,7 @@ AGENT = PittRASDrone
 CONTROLLER = WaypointDemoController
 
 TASKS = {
+    'HoldPositionTask': HoldPositionTask,
     'XYZTranslationTask': XYZTranslationTask,
     'TakeoffTask': TakeoffTask
 }
